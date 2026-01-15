3e68a0658ceff48c5dd292b89cf3bb5b6a67ecb1
laurenippolito33 <laurenippolito33@gmail.com>
2026-01-15 17:30:50 -0500

    Add files via upload

--- lab9/main.c
+++ lab9/main.c
@@ -1,62 +1,97 @@
 /*
- * GccApplication1.c
+ * lab9practicetry2.c
  *
- * Created: 3/28/2025 12:04:56 AM
+ * Created: 3/28/2025 1:40:13 AM
  * Author : Lauren Ippolito
  */ 
 
+
 #include <avr/io.h>
 #define FREQ_CLK 1000000
-void wait(volatile int multiple);
+void wait(volatile int);
 int sendtomax(unsigned char command, unsigned char data);
-
 
 int main(void)
 {
-   //set up 
-  DDRD = 0xFF;   // Set DDRD to display data that comes in from Secondary
-  DDRB = 0b00101100; //	DDRB = 1<<PORTB5 | 1<<PORTB3 | 1<<PORTB2;  // Set pins SCK, MOSI, and SS as output
-   //initialize SPI to operate in main mode 0
-  SPCR = 0b01010001; // SPCR = 1<<SPE | 1<<MSTR | 1<<SPR0; // (SPIE = 0, SPE = 1, DORD = 0, MSTR = 1, CPOL = 0, CPHA = 0, SPR1 = 0, SPR0 = 1)
-					 // enable the SPI, set to Main mode 0, SCK = Fosc/16, lead with MSB
-   //enable/disable decoding mode for desired digits
-   char decode1 = 0b00001001;
-   char decode2 = 0b00000011l;
-   sendtomax(decode1,decode2);
-   //set scan limit (number of digits on display)
-   char scan1 = 0b00001011;
-   char scan2 = 0b00000010;
-   sendtomax(scan1,scan2);
-   //turn on display
-   char turnon1 = 0b00001100;
-   char turnon2 = 0b00000001;
-   sendtomax(turnon1,turnon2);
-   
-   char  screen1=0b00000001;//use to set screen 1
-   char  screen2=0b00000010;//use to set screen 2
-   while(1) //send values for digits
-   {
-	   
-	   //display month 03 for 1 second
-	   char month0= 0b00000000;//0
-	   char month3=0b00000011;//3
-	   sendtomax(screen1,month0); //set screen 1 to 0
-	   sendtomax(screen2,month3); //set screen 2 to 3
-	   wait(1000); //wait 1s
-	   //display the day 29 for 1 second
-	   char date2= 0b00000010;//2
-	   char date9= 0b00001111;//9
-	   sendtomax(screen1,date2);//set screen 1 to 2
-	   sendtomax(screen2,date9);//set screen 2 to 9
-	   wait(1000);//wait 1s
-	   //blank display for 2 seconds
-	   char blank=0b00001111;//blank
-	   sendtomax(screen1,blank);
-	   sendtomax(screen2,blank);
-	   wait(2000);
-	   
-   }//repeat indefinably
-   
+	
+	DDRD = 0xFF;   // Set DDRD to display data that comes in from Secondary
+	DDRB = 0b00101100; //	DDRB = 1<<PORTB5 | 1<<PORTB3 | 1<<PORTB2;  // Set pins SCK, MOSI, and SS as output
+	//initialize SPI
+	SPCR = 0b01010001; // SPCR = 1<<SPE | 1<<MSTR | 1<<SPR0; // (SPIE = 0, SPE = 1, DORD = 0, MSTR = 1, CPOL = 0, CPHA = 0, SPR1 = 0, SPR0 = 1)
+					// enable the SPI, set to Main mode 0, SCK = Fosc/16, lead with MSB
+	//enable decode mode
+	char decode1 = 0b00001001;
+	char decode2 = 0b00000011;
+	sendtomax(decode1,decode2);
+	//set scan limit
+	char scan1 = 0b00001011;
+	char scan2 = 0b00000010;
+	sendtomax(scan1, scan2);
+	//turn on delay
+	char turnon1 = 0b00001100;
+	char turnon2 = 0b00000001;
+	sendtomax(turnon1, turnon2);
+	
+	
+	char screen1 = 0b00000001; // screen 1
+	char screen2 = 0b00000010; // screen2
+	
+	while (1)
+	{
+		
+		// display month
+		char month0 = 0b00000000; // this is the data to display a zero
+		char month3 = 0b00000011; // this will display a 3
+		sendtomax(screen1, month0);
+		sendtomax(screen2, month3);
+		// wait 1 sec
+		wait(1000);
+		// display day
+		char date2 = 0b00000010; // displays a 2
+		char date8 = 0b00001000; // displays an 8
+		sendtomax(screen1, date2);
+		sendtomax(screen2, date8);
+		wait(1000);
+		// display blank
+		char blankdata= 0b00001111; // displays blank
+		sendtomax(screen1,blankdata);
+		sendtomax(screen2,blankdata);
+		wait(2000);
+	}
+	
+}
+
+
+void wait(volatile int number_of_msec)
+{
+	char register_B_setting;
+	char count_limit;
+	
+	// allows for different clock frequencies and sets the settings and number of counts it takes to his 1 ms
+	switch(FREQ_CLK) {
+		case 16000000:
+		register_B_setting = 0b00000011;
+		count_limit = 250;
+		break;
+		case 8000000:
+		register_B_setting = 0b00000011;
+		count_limit = 125;
+		break;
+		case 1000000:
+		register_B_setting = 0b00000010;
+		count_limit = 125;
+		break;
+	}
+	// TIME KILLING LOOP
+	while(number_of_msec > 0)
+	{
+		TCCR0A = 0x00;
+		TCNT0 = 0; // clears timer register at start
+		TCCR0B = register_B_setting; // starts timer with the correct settings from above
+		while (TCNT0 < count_limit); // timer counts until it hits enough counts to be 1 ms
+		TCCR0B = 0x00; // stops timer
+		number_of_msec--; // keeps count of number of ms that are left to go
+	}
 }
 
 int sendtomax(unsigned char command, unsigned char data)
@@ -72,36 +107,3 @@ int sendtomax(unsigned char command, unsigned char data)
 	return 0;
 }
 
-
-
-void wait(volatile int number_of_msec) {
-	// This subroutine creates a delay equal to number_of_msec*T, where T is 1 msec
-	// It changes depending on the frequency defined by FREQ_CLK
-	char register_B_setting;
-	char count_limit;
-	
-	// Some typical clock frequencies:
-	switch(FREQ_CLK) {
-		case 16000000:
-		register_B_setting = 0b00000011; // this will start the timer in Normal mode with prescaler of 64 (CS02 = 0, CS01 = CS00 = 1).
-		count_limit = 250; // For prescaler of 64, a count of 250 will require 1 msec
-		break;
-		case 8000000:
-		register_B_setting =  0b00000011; // this will start the timer in Normal mode with prescaler of 64 (CS02 = 0, CS01 = CS00 = 1).
-		count_limit = 125; // for prescaler of 64, a count of 125 will require 1 msec
-		break;
-		case 1000000:
-		register_B_setting = 0b00000010; // this will start the timer in Normal mode with prescaler of 8 (CS02 = 0, CS01 = 1, CS00 = 0).
-		count_limit = 125; // for prescaler of 8, a count of 125 will require 1 msec
-		break;
-	}
-	
-	while (number_of_msec > 0) {
-		TCCR0A = 0x00; // clears WGM00 and WGM01 (bits 0 and 1) to ensure Timer/Counter is in normal mode.
-		TCNT0 = 0;  // preload value for testing on count = 250
-		TCCR0B =  register_B_setting;  // Start TIMER0 with the settings defined above
-		while (TCNT0 < count_limit); // exits when count = the required limit for a 1 msec delay
-		TCCR0B = 0x00; // Stop TIMER0
-		number_of_msec--;
-	}
-} // end wait()
